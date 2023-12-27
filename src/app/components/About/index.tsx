b620de774ef3c76914fc37f2d11ea760ff865da9
Amanda Carvalho <carvalhotp.amanda@gmail.com>
2023-12-27 09:53:03 -0300

    starting portfolio project

---
+++ src/app/components/About/index.tsx
@@ -0,0 +1,7 @@
+const About = () => {
+
+    return <>
+    <h1>Sobre mim</h1>
+    <p>sei que la sei que la sei que la sei que la sei que la sei que la sei que la sei que la sei que la sei que la sei que la sei que la sei que la </p>
+    </>
+}
